c9b8b1c223ca7a76cdd7cf0b7ffeca43880ab058
Seth Schoenert <seth.schoenert@gmail.com>
2023-06-28 11:54:14 -0500

    completed config, authentication, and controllers

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,64 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// singup user ('/api/user)
+router.post('/', async (req, res) => {
+    try {
+        const dbUserData = await User.create(req.body);
+        req.session.save(() => {
+            req.session.userId = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+            res.status(201).json({ message: `Account created for ${dbUserData.username}`});
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// login user ('/api/user/login')
+router.post('/login', async (req, res) => {
+    try {
+        const dbUserData = await User.findOne({
+            where: {username: req.body.username}
+        });
+        if (!dbUserData) {
+            res.status(400).json({ message: `User id ${req.params.id} is not valid.` });
+            return;
+        }
+        // check pw
+        const pwValidated = await dbUserData.checkPassword(req.body.password)
+        if (!pwValidated) {
+            res.status(400).json({ message: "Incorrect password!" });
+            return;
+        }
+        // create session and send response back
+        req.session.save(() => {
+            req.session.userId = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;        
+        //send response to client
+        res.status(200).json({ message: "You are logged in!" });
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// logout user ('/api/user/logout')
+router.post('/logout', withAuth, async (req, res) => {
+    try {
+        if (req.session.loggedIn) {
+            const dbUserData = await req.session.destroy(() => {
+                res.status(204).end();
+            });
+        } else {
+            res.status(404).end();
+        }
+    } catch {
+        res.status(400).end();
+    }
+});
+
+module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,89 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+const sequelize = require('../config/config');
+
+// Get all posts ('/')
+router.get('/', async (req, res) => {
+    try {
+          // Retrieve all posts from db
+        const dbPostData = await Post.findAll({ 
+            attributes: ['id', 'title', 'content', 'created_at'],           
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment', 'postId', 'userId', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username'],
+                    },
+                },
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+            order: [['created_at', 'DESC']],
+        })
+        // Serialize data retrieved
+        const posts = dbPostData.map((post) => post.get({ plain: true }));
+        console.log(posts)
+        // Respond with template to render along with date retrieved
+        res.render('homepage', 
+            { posts, 
+            loggedIn: req.session.loggedIn, 
+            username: req.session.username,
+            userId: req.session.userId });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Get single post ('/post/:id')
+router.get('/post/:id', async (req, res) => {
+    try{
+        const dbPostData = await Post.findOne({
+            where: {id: req.params.id},
+            attributes: ['id', 'content', 'title', 'created_at'],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment', 'postId', 'userId', 'created_at'],
+                    include: {
+                      model: User,
+                      attributes: ['username'],
+                    },
+                  },
+                  {
+                    model: User,
+                    attributes: ['username'],
+                  },
+            ],
+        });
+        if (dbPostData) {
+            const post = dbPostData.get({ plain: true });
+            console.log(post);
+            res.render('single-post', { post, loggedIn: req.session.loggedIn, username: req.session.username, })  
+        } else {
+            res.status(404).json({ message: "This id has no post."});
+            return;
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }   
+});
+
+// Login
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+// Signup
+router.get('/signup', async (req, res) => {
+    res.render('signup');
+})
+
+module.exports = router; 
